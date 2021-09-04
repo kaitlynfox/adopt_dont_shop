b66194c8244b39730bd0dff87eb50fb34f778258
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-09-04 15:19:57 -0600

    Add additional validation test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -12,5 +12,6 @@ RSpec.describe Application, type: :model do
     it {should validate_presence_of(:city)}
     it {should validate_presence_of(:state)}
     it {should validate_presence_of(:zip_code)}
+    it {should validate_presence_of(:application_status)}
   end
 end
