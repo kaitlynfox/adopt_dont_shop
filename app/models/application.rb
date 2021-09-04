b516fd1130786ca88d8b65e78074e28444ddde08
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-09-04 15:20:37 -0600

    Add validation for application_status

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,6 +4,7 @@ class Application < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true
+  validates :application_status, presence: true
 
   has_many :application_pets
   has_many :pets, through: :application_pets
